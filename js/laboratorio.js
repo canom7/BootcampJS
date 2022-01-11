8a20c165fce69a9775421f6f227884f04a71ef18
Mire <58038175+canom7@users.noreply.github.com>
2022-01-11 13:38:20 +0100

    Delete js directory

--- js/laboratorio.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("avatar").src="/img/avatar.jpg";
-document.getElementById("name").value = "Mi nombre";
-
-var lastname= document.getElementById("lastname").value = "Mi apellido";
-
-document.querySelector("#update").addEventListener('click', function(event){
-    console.log(lastname);
-})
